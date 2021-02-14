ab22c0958ea97f71e315ee1fa9a7a03cc969ecd4
andrea nonni <a.nonni.1985@gmail.com>
2021-02-14 01:06:54 +0100

    Struttura Trade e Setup di Ingresso

--- IKH/IKH.cs
+++ IKH/IKH.cs
@@ -5,53 +5,118 @@ using cAlgo.API.Indicators;
 using cAlgo.API.Internals;
 using System.Collections.Generic;
 using cAlgo.Indicators;
-using System.Linq;
 
 namespace cAlgo.Robots
 {
+    public class Globals
+    {
+        public const String Strategy = "IKH";
+        public const String SignalSSBSR = "SSB STOP&REVERSE";
+    }
+    public class Setup
+    {
+        public bool BreakEvenOnFirstTP;
+        public bool BreakEvenOnPipsGain;
+        public double BreakEvenPipsGain;
+
+        public bool PartialClosureOnFirstTP;
+        public double PartialClosurePerc;
+
+        public bool TrailingStopOnOpen;
+        public bool TrailingStopOnFirstTP;
+        public bool TrailingStopOnPipsGain;
+        
+        public Setup() {
+        }
+    }
     public class Trade
     {
-        public Position POS;
-        //prezzo di take profit
-        public double TP1Price;
-        //pips di teke profit
+        private List<Position> PositionList;
+        public Setup Setup;
+        public TradeType Type;
         public double TP1;
-        //prezzo di take profit
-        public double TP2Price;
-        //pips di teke profit
         public double TP2;
-        //flag TP1 colpito
         public bool TP1Striked;
-        //flag TP2 colpito
         public bool TP2Striked;
-        //prezzo di StopLoss
-        public double SLPrice;
-        //pips di StopLoss
         public double SL;
+        public double Volume;
+        public string Signal;
 
-        public Trade()
+        public int PositionCount
         {
+            get { return PositionList.Count(); }
+        }
+        public double PositionEntryPrice {
+            get {
+                switch(PositionList.Count) {
+                    case 1:
+                        return PositionList[0].EntryPrice;
+                    case 0:
+                        return 0;
+                    default :
+                        return 0;
+                }
+            }
+        }
+        public Trade(IKH Robot)
+        {
+            Robot.Positions.Closed += OnClosePosition;
+            PositionList = new List<Position>();
             TP1Striked = false;
             TP2Striked = false;
         }
-
-        public void DoOrder(IKH Robot)
+        public bool OpenMarket(IKH Robot)
         {
-            TradeResult Result = Robot.ExecuteMarketOrder(TradeType.Buy, Robot.Symbol.Name, Robot.Volume, IKH.Strategy, SL, null, IKH.Strategy);
+            TradeResult Result = Robot.ExecuteMarketOrder(Type, Robot.Symbol.Name, Volume, Signal, SL, null, Globals.Strategy);
             if (Result.IsSuccessful)
             {
-                POS = Result.Position;
+                PositionList.Add(Result.Position);
+                return true;
             }
+            return false;
+        }
+        public void Close()
+        {
+            foreach (Position x in PositionList)
+            {
+                x.Close();
+            }
+        }
+        public void SetBreakEven(double pips) {
+            foreach(Position x in PositionList) {
+                x.ModifyStopLossPips(pips);
+            }
+        }
+        public void SetTrailingStop(bool activate) {
+            foreach (Position x in PositionList) {
+                x.ModifyTrailingStop(activate);
+            }
+        }
+        public void ClosePartialPosition() {
+            switch (PositionList.Count) {
+                case 1:
+                    PositionList[0].ModifyVolume(PositionList[0].VolumeInUnits * Setup.PartialClosurePerc);
+                    break;
+                case 0:
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        public void OnClosePosition(PositionClosedEventArgs e)
+        {
+            PositionList.RemoveAll(ra => ra.Id == e.Position.Id);
         }
     }
 
     [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
     public class IKH : Robot
     {
-        public const String Strategy = "IKH";
-
         [Parameter("Stop Loss Minimo", Group = "Parametri Strategia", DefaultValue = 10.0, MinValue = 1)]
         public double MinSL { get; set; }
+        [Parameter("BreakEven Pips", Group = "Parametri Strategia", DefaultValue = 1.0, MinValue = 0.01)]
+        public double BEPips { get; set; }
         [Parameter("Volume", Group = "Parametri Strategia", DefaultValue = 10000, MinValue = 0.1)]
         public double Volume { get; set; }
         [Parameter("Tenkan Sen", Group = "Periodi Ichimoku", DefaultValue = 9, MinValue = 2)]
@@ -63,25 +128,10 @@ namespace cAlgo.Robots
 
         public IchimokuKinkoHyo IKHIndicator;
 
-        //prezzo di take profit
-        public double TP1Price;
-        //pips di teke profit
-        public double TP1;
-        //prezzo di take profit
-        public double TP2Price;
-        //pips di teke profit
-        public double TP2;
-        //flag TP colpito
-        public bool ProfitStriked;
-        //prezzo di StopLoss
-        public double SLPrice;
-        //pips di StopLoss
-        public double SL;
 
         //orientamento del prezzo (sopra o sotto la Senkou Span B)
-        public TradeType Direction;
-        public TradeType LastDirection;
-        public List<Trade> TradeList;
+        private TradeType LastDirection;
+        private List<Trade> TradeList;
 
 
         protected override void OnStart()
@@ -91,47 +141,50 @@ namespace cAlgo.Robots
         }
         protected override void OnTick()
         {
-            //logica TakeProfit , BreakEven , Trail Stop
-
-            //se il prezzo colpisce il primo profit prendo metà profitto e lascio correre il resto mettendo a BreakEven
-            if (Positions.Count == 1 && !ProfitStriked)
-            {
-                if (Positions[0].TradeType == TradeType.Buy && Symbol.Bid >= TP1Price)
-                {
-                    Positions[0].ModifyVolume(Positions[0].VolumeInUnits / 2);
-                    Positions[0].ModifyStopLossPips(-1);
-                    Positions[0].ModifyTrailingStop(true);
-                    ProfitStriked = true;
-
-
+            bool TP1Striked = false;
+            //gestione della posizione
+            foreach(Trade x  in TradeList) {
+                if (!x.TP1Striked && x.Type == TradeType.Buy && Symbol.Bid >= PricePlusPips(x.PositionEntryPrice, x.TP1)) {
+                    x.TP1Striked = true;
+                    TP1Striked = true;
                 }
-                else if (Positions[0].TradeType == TradeType.Sell && Symbol.Bid <= TP1Price)
-                {
-                    Positions[0].ModifyVolume(Positions[0].VolumeInUnits / 2);
-                    Positions[0].ModifyStopLossPips(-1);
-                    Positions[0].ModifyTrailingStop(true);
-                    ProfitStriked = true;
+                if (!x.TP1Striked && x.Type == TradeType.Sell && Symbol.Bid <= PricePlusPips(x.PositionEntryPrice, x.TP1 * -1)) {
+                    x.TP1Striked = true;
+                    TP1Striked = true;
+                }
+                if (x.Setup.BreakEvenOnFirstTP && TP1Striked) {
+                    x.SetBreakEven(BEPips);
+                }
+                if (x.Setup.TrailingStopOnFirstTP && TP1Striked) {
+                    x.SetTrailingStop(true);
+                }
+                if (x.Setup.PartialClosureOnFirstTP && TP1Striked) {
+                    x.ClosePartialPosition();
                 }
             }
         }
         protected override void OnBar()
         {
+            //direzione del prezzo
+            TradeType Direction;
+            //prezzo di take profit
+            double TP1Price;
+            //prezzo di take profit 2
+            double TP2Price;
+            //prezzo di StopLoss
+            double SLPrice;
             //prezzo del livello senkou Span A
-            double SPA = IKHIndicator.SenkouSpanA.Last(Kijun + 2);
+            double SPA = IKHIndicator.SenkouSpanA.Last(Kijun + 1);
             //prezzo del livello senkou Span B
-            double SPB = IKHIndicator.SenkouSpanB.Last(Kijun + 2);
-            //prezzo di apertura barra corrente
-            double Open = Bars.Last(1).Open;
-            //prezzo di apertura barra precedente
-            double OpenBefore = Bars.Last(2).Open;
+            double SPB = IKHIndicator.SenkouSpanB.Last(Kijun + 1);
             //prezzo di chiusura barra corrente
             double Close = Bars.Last(1).Close;
-            //osservatore execute order
-            TradeResult Result;
-
-            //
-            //stabilisco al direzione attuale del prezzo
-            //
+            //variabili temporanee
+            double tmpTP1;
+            double tmpSL;
+            //*************************************************
+            //stabilisco la direzione attuale del prezzo
+            //*************************************************
             if (Close > SPB)
             {
                 Direction = TradeType.Buy;
@@ -140,85 +193,96 @@ namespace cAlgo.Robots
             {
                 Direction = TradeType.Sell;
             }
-            //
-            //gestisco lo stop di chiusura candela
-            //
-            //if (Positions.Count == 1) {
-            //    if (Positions[0].TradeType == TradeType.Buy) {
-            //        Positions[0].Close();
-            //    }
-            //    else if (Positions[0].TradeType == TradeType.Sell) {
 
-            //    }
-            //}
-
-
-            //if (SPB > SPA && Close >= Open && Close > SPB && Math.Min(Open, OpenBefore) <= SPB) {
+            //********************
+            //SEGNALE REVERSE LONG
+            //********************
+            // se c'è stato un cambio di direzione del prezzo ed ora è in buy e la kumo era ribassista
             if (SPB > SPA && Direction != LastDirection && Direction == TradeType.Buy)
             {
-                //if (Direction != LastDirection && Direction == TradeType.Buy) {
-                // caso in cui la KUMO ribassista viene tagliata al rialzo dal prezzo 
-
-                //stop and reverse
-                if (Positions.Count == 1)
+                // chiudo l'eventuale SHORT aperto
+                foreach (Trade x in TradeList.Where(c => c.Signal == Globals.SignalSSBSR && c.PositionCount > 0 && c.Type == TradeType.Sell))
                 {
-                    if (Positions[0].TradeType == TradeType.Sell)
-                    {
-                        Positions[0].Close();
-                    }
+                    x.Close();
                 }
-                if (Positions.Count == 0)
+                // se non ho già il LONG aperto per questo segnale allora apro il LONG
+                if (!TradeList.Exists(e => e.Signal == Globals.SignalSSBSR && e.Type == TradeType.Buy && e.PositionCount > 0))
                 {
-
                     SLPrice = SPA - (Symbol.Spread * 2);
                     TP1Price = Close + Math.Abs(Symbol.Bid - SLPrice);
-                    SL = Math.Max(DistanceInPips(SLPrice), MinSL);
-                    TP1 = DistanceInPips(TP1Price);
-
-                    ExecuteMarketOrder(TradeType.Buy, Symbol.Name, Volume, Strategy, SL, null, Strategy);
-                    ProfitStriked = false;
-
-
-                    //Trade trade = new Trade() {
-                    //    SLPrice = SPA - (Symbol.Spread * 2),
-                    //    TP1Price = Close + Math.Abs(Symbol.Bid - SLPrice),
-                    //    SL = Math.Max(DistanceInPips(SLPrice), MinSL),
-                    //    TP1 = DistanceInPips(TP1Price)
-                    //};
-                    //trade.DoOrder(this);
-                    //TradeList.Add(trade);
-
-
-                }
-                //} else if (SPA > SPB && Close >= Open && Close > SPB && Math.Min(Open, OpenBefore) <= SPB) {
-                //} else if(SPB < SPA && Close <= Open && Close < SPB && Math.Max(Open, OpenBefore) >= SPB) {
-            }
-            else if (SPB < SPA && Direction != LastDirection && Direction == TradeType.Sell)
-            {
-                //else if (Direction != LastDirection && Direction == TradeType.Sell) {
-                // caso in cui la KUMO rialzista viene tagliata al ribasso dal prezzo 
-
-                //stop and reverse
-                if (Positions.Count == 1)
-                {
-                    if (Positions[0].TradeType == TradeType.Buy)
+                    tmpSL = Math.Max(DistanceInPips(SLPrice), MinSL);
+                    tmpTP1 = DistanceInPips(TP1Price);
+                    Trade trade = new Trade(this) {
+                        Type = TradeType.Buy,
+                        Volume = Volume,
+                        SL = tmpSL,
+                        TP1 = tmpTP1,
+                        Signal = Globals.SignalSSBSR,
+                        Setup = new Setup() {
+                            BreakEvenOnFirstTP = true
+                            ,TrailingStopOnFirstTP = true
+                            ,PartialClosureOnFirstTP = true
+                            ,PartialClosurePerc = 0.5
+                        }
+                    };
+                    if (trade.OpenMarket(this))
                     {
-                        Positions[0].Close();
+                        TradeList.Add(trade);
+                    }
+                    else
+                    {
+                        Print("Errore Apertura Posizione Long");
                     }
                 }
-                if (Positions.Count == 0)
+            }
+            //********************
+            //SEGNALE REVERSE SHORT
+            //********************
+            // se c'è stato un cambio di direzione del prezzo ed ora  in sell e la kumo era rialzista
+            else if (SPB < SPA && Direction != LastDirection && Direction == TradeType.Sell)
+            {
+                //chiudo l'eventuale LONG
+                foreach (Trade x in TradeList.Where(c => c.Signal == Globals.SignalSSBSR && c.PositionCount > 0 && c.Type == TradeType.Buy))
+                {
+                    x.Close();
+                }
+                // se non ho già lo SHORT aperto per questo segnale allora apro lo SHORT
+                if (!TradeList.Exists(e => e.Signal == Globals.SignalSSBSR && e.Type == TradeType.Sell && e.PositionCount > 0))
                 {
                     SLPrice = SPA + (Symbol.Spread * 2);
                     TP1Price = Close - Math.Abs(Symbol.Bid - SLPrice);
-                    SL = Math.Max(DistanceInPips(SLPrice), MinSL);
-                    TP1 = DistanceInPips(TP1Price);
+                    tmpSL = Math.Max(DistanceInPips(SLPrice), MinSL);
+                    tmpTP1 = DistanceInPips(TP1Price);
+                    Trade trade = new Trade(this) 
+                    {
+                        Type = TradeType.Sell,
+                        Volume = Volume,
+                        SL = tmpSL,
+                        TP1 = tmpTP1,
+                        Signal = Globals.SignalSSBSR,
+                        Setup = new Setup() {
+                            BreakEvenOnFirstTP = true
+                            ,TrailingStopOnFirstTP = true
+                            ,PartialClosureOnFirstTP=true
+                            ,PartialClosurePerc = 0.5
+                        }
+                    };
 
-                    Result = ExecuteMarketOrder(TradeType.Sell, Symbol.Name, Volume, Strategy, SL, null, Strategy);
-
-                    ProfitStriked = false;
+                    if (trade.OpenMarket(this))
+                    {
+                        TradeList.Add(trade);
+                    }
+                    else
+                    {
+                        Print("Errore Apertura Posizione Short");
+                    }
                 }
             }
             LastDirection = Direction;
+        }
+        protected override void OnStop()
+        {
+
         }
         public double DistanceInPips(double price)
         {
@@ -226,9 +290,8 @@ namespace cAlgo.Robots
             double pips = delta / Symbol.PipSize;
             return Math.Round(pips, 0);
         }
-        protected override void OnStop()
-        {
-
+        public double PricePlusPips(double price, double pips) {
+            return price + (pips * Symbol.PipValue);
         }
     }
 }
